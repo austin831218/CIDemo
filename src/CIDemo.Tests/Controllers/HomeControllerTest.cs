90e97d36773527bcc2b2384034c6215f9b6e3463
Zhao Peng <zpisgod@gmail.com>
2016-03-27 14:30:53 +0800

    init project

---
+++ src/CIDemo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CIDemo;
+using CIDemo.Controllers;
+
+namespace CIDemo.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void GetConfig()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ContentResult result = controller.Test() as ContentResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Test", result.Content);
+            
+        }
+    }
+}
---
+++ src/CIDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CIDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return Content(System.Configuration.ConfigurationManager.AppSettings["test"]);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Test()
+        {            
+            return Content("Test");
+        }
+    }
+}
